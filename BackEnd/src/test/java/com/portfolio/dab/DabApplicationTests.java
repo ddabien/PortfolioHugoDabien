9b83b21036f69a93bd72ed25342a0e8a35159521
Hugo Dabien <104176100+ddabien@users.noreply.github.com>
2022-07-15 00:51:09 -0300

    Initial commit

---
+++ BackEnd/src/test/java/com/portfolio/dab/DabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.dab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
