80a9a03a6cb8e5fe4ca174b33965305683211f75
Hugo Dabien <104176100+ddabien@users.noreply.github.com>
2022-07-19 18:22:19 -0300

    algunos cambios

--- FrontEnd/src/app/models/persona.model.ts
+++ FrontEnd/src/app/models/persona.model.ts
@@ -1,12 +1,10 @@
-import { NumberSymbol } from "@angular/common";
-
 export class persona {
-    id?: Number;
+    id?: number;
     nombre: String;
     apellido: String;
     img: String;
 
-    constructor (nombre: String, apellido: String, img: String){
+    constructor (nombre: String, apellido: String, img: String) {
         this.nombre = nombre;
         this.apellido = apellido;   
         this.img= img;
