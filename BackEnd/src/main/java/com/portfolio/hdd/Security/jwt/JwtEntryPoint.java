ff85c0fbaa309cec4f335389396109d393aefc76
Hugo Dabien <104176100+ddabien@users.noreply.github.com>
2022-08-09 19:17:34 -0300

    creación del JWT
    Avanzo con lentitud por dificultades en algunas librerias

---
+++ BackEnd/src/main/java/com/portfolio/hdd/Security/jwt/JwtEntryPoint.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.hdd.Security.jwt;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author drpendejoloco
+ * Esta clase va a chequear un toquen v'alido
+ */
+@Component
+public class JwtEntryPoint implements AuthenticationEntryPoint{
+    private final static Logger logger = LoggerFactory.getLogger(JwtEntryPoint.class);
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+        logger.error("Fallo de metodo commence");
+        response.sendError(HttpServletResponce.SC_UNAUTHORIZED);
+    }
+
